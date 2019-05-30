8c6947bf1c05ab700da288a8981dc8a9bb68f0d6
emily baier <emilyrbaier@gmail.com>
2019-05-30 14:31:33 -0700

    refactored to use promise middleware rather than thunk middleware

--- src/actions/charactersActions.js
+++ src/actions/charactersActions.js
@@ -1,25 +1,10 @@
 import { getCharacters } from '../services/lastAirbenderApi';
+import { createAction } from 'promise-middleware-redux';
 
-export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
-export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
-export const FETCH_CHARACTERS_ERROR = 'FETCH_CHARACTERS_ERROR';
-
-export const fetchCharacters = () => dispatch => {
-  dispatch({
-    type: FETCH_CHARACTERS_LOADING
-  });
-
-  return getCharacters()
-    .then(characters => {
-      dispatch({
-        type: FETCH_CHARACTERS,
-        payload: characters
-      });
-    })
-    .catch(err => {
-      dispatch({
-        type: FETCH_CHARACTERS_ERROR,
-        payload: err
-      });
-    });
-};
+export const [
+  fetchCharacters,
+  FETCH_CHARACTERS,
+  FETCH_CHARACTERS_LOADING,
+  FETCH_CHARACTERS_FULFILLED,
+  FETCH_CHARACTERS_ERROR
+] = createAction('FETCH_CHARACTERS', getCharacters);
--- src/actions/charactersActions.test.js
+++ src/actions/charactersActions.test.js
@@ -1,21 +1,15 @@
-import { fetchCharacters, FETCH_CHARACTERS_LOADING, FETCH_CHARACTERS } from '../actions/charactersActions';
+import { fetchCharacters, FETCH_CHARACTERS_LOADING, FETCH_CHARACTERS, FETCH_CHARACTERS_ERROR, FETCH_CHARACTERS_FULFILLED } from '../actions/charactersActions';
 
 jest.mock('../services/lastAirbenderApi.js');
 
 describe('character actions', () => {
   it('it dispatches a loading action and a fetch characters action', () => {
-    const thunk = fetchCharacters();
-    const dispatch = jest.fn();
-
-    return thunk(dispatch)
-      .then(() => {
-        expect(dispatch).toHaveBeenCalledWith({
-          type: FETCH_CHARACTERS_LOADING
-        });
-        expect(dispatch).toHaveBeenCalledWith({
-          type: FETCH_CHARACTERS,
-          payload: []
-        });
-      });
+    expect(fetchCharacters()).toEqual({
+      type: FETCH_CHARACTERS,
+      pendingType: FETCH_CHARACTERS_LOADING,
+      rejectedType: FETCH_CHARACTERS_ERROR,
+      fulfilledType: FETCH_CHARACTERS_FULFILLED,
+      payload: expect.any(Promise)
+    });
   });
 });
